5ba3cdc3ee3fed73c05e4349d95cfdcc32fa0fc0
vkiz <vk.fstor@gmail.com>
2017-11-21 00:17:51 +0400

    Update Utils class

--- src/main/java/com/opencode/app/utils/Utils.java
+++ src/main/java/com/opencode/app/utils/Utils.java
@@ -1,13 +1,38 @@
+/*
+ * Copyright (C) 2017 Open Code.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "as is" basis,
+ * without warranties or conditions of any kind, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.opencode.app.utils;
 
 import java.math.BigInteger;
 import java.security.MessageDigest;
 
 /**
+ * Класс с вспомогательными методами.
  *
+ * @version 1.0
+ * @author Vladimir Kizelbashev
  */
 public class Utils {
 
+    /**
+     * Метод возвращает MD5 в виде строки из 32 шестнадцатеричных символов.
+     * @param str Исходная строка
+     * @return Строка MD5 на основе исходной
+     * @throws Exception
+     */
     public static String getMD5Hash(String str) throws Exception {
         MessageDigest msgDigest = MessageDigest.getInstance("MD5");
         msgDigest.reset();
